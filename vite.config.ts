9864f521245a071f48ff3e76bc265257389d9e89
zhangrui <aioloszh092@163.com>
2024-09-14 21:45:37 +0800

    feat: 新增eslint代码检验和prettier的格式化规则

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), // eslint插件配置
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.ts'], // 指定需要检查的文件
+      exclude: ['node_modules/**', 'dist/**'], // 指定不需要检查的文件
+      fix: true, // 是否自动修复
+      cache: false // 是否启用缓存
+    })
+  ]
 })
